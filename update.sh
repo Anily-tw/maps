13f6dd3b35cc9e15a4a74680cc57eeab4ed33264
Paul <paul.turkovskiy@gmail.com>
2024-09-10 12:55:08 +0300

    fix autoupload

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-REPO_DIR="~/servers/maps"
 COMMIT_MSG="Auto-upload new maps"
 BRANCH="master"
-
-cd "$REPO_DIR" || { echo "Directory not found: $REPO_DIR"; exit 1; }
 
 git pull origin "$BRANCH"
 git add .
